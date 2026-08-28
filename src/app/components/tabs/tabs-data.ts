81f4a6daeec94fcc77019f54e7f991fba5f03995
alessia <alessia.zacco12@gmail.com>
2026-08-28 15:46:40 +0200

    Fix: Add tabs data

---
+++ src/app/components/tabs/tabs-data.ts
@@ -0,0 +1,25 @@
+// Definisco la struttura di un Tab.
+export type Tab = {
+  title: string;
+  content: string;
+};
+
+// Dati dei Tab.
+export const TABS: Tab[] = [
+  {
+    title: 'First Tab',
+    content: 'First Tab content to be displayed here.',
+  },
+  {
+    title: 'Second Tab',
+    content: 'Second Tab content to be displayed here.',
+  },
+  {
+    title: 'Third Tab',
+    content: 'Third Tab content to be displayed here.',
+  },
+  {
+    title: 'Fourth Tab',
+    content: 'Fourth Tab content to be displayed here.',
+  },
+];
